512a47cb4ab1f999b2363d08507ffa27e901f693
Abdul Basit Asghar <asgharabdulbasit@gmail.com>
2024-07-14 22:37:58 +0500

    Data Types

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,31 @@
 "use strict";
 console.log('Hello World');
 let age = 20;
+if (age < 50)
+    age += 10;
+console.log('Age after addition of 10: ', age);
+let sales = 123456789;
+let course = 'TypeScript';
+let is_published = true;
+let level;
+function render(discription, document) {
+    console.log(discription + ': ' + document);
+}
+render('Value in number Data Types', sales);
+render('Value in string Data Types', course);
+render('Value in boolean Data Types', is_published);
+let number = [1, 2, 3];
+render('Value in array Data Types', number);
+let user = [1, '2'];
+render('Value in tuple Data Types', user);
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+let mySize = Size.Small;
+render('Sizes Available', '');
+console.log(Size);
+render('Size selected', mySize);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,36 @@
 console.log('Hello World');
-let age:  number = 20;
+// Declaring in TypeScript.
+let age: number = 20;
 if (age < 50)
     age += 10;
-console.log(age);
+console.log('Age after addition of 10: ', age);
+
+// DataTypes in TypeScript
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published = true;
+let level; // Type: any
+function render(discription: string, document: any){
+    console.log(discription + ': ' + document)
+}
+render('Value in number Data Types', sales);
+render('Value in string Data Types', course);
+render('Value in boolean Data Types', is_published);
+
+// Arrays in TypeScript
+let number: number[] = [1, 2, 3];
+render('Value in array Data Types', number);
+
+// Tuple(fixed length Array) in TypeScript
+let user: [number, string] = [1, '2'];
+render('Value in tuple Data Types', user);
+
+// Enums(List of related constants)
+// const small = 1;
+// const medium = 2;
+// const large = 3;
+enum Size {Small = 1, Medium, Large};
+let mySize: Size = Size.Small;
+render('Sizes Available', '');
+console.log(Size)
+render('Size selected', mySize);
